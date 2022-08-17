d178d6efea0ad6c617f44a5864ac1242ca36fba3
Aleksey G <gelvides.aleksey@outlook.com>
2022-08-17 15:34:06 +0700

    Создание проекта

---
+++ src/main/java/com/gelvides/ttforlanit/TtforlanitApplication.java
@@ -0,0 +1,13 @@
+package com.gelvides.ttforlanit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtforlanitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtforlanitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gelvides/ttforlanit/TtforlanitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelvides.ttforlanit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtforlanitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
